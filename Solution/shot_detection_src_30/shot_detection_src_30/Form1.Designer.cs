b02ee0b9e17898af6da808e3454376c72214f30a
Patrick Van Staey <patrickvanstaey@gmail.com>
2014-11-07 17:41:29 +0100

    play, stop, pause, browse

--- Solution/shot_detection_src_30/shot_detection_src_30/Form1.Designer.cs
+++ Solution/shot_detection_src_30/shot_detection_src_30/Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace shot_detection_src_30
+﻿using DirectShowLib;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace shot_detection_src_30
 {
     partial class Form1
     {
@@ -13,9 +18,17 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            // Make sure to release the DxPlay object to avoid hanging
+            if (m_play != null)
             {
-                components.Dispose();
+                m_play.Dispose();
+            }
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
             }
             base.Dispose(disposing);
         }
@@ -28,22 +41,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(12, 326);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(13, 307);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(55, 13);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "File name:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtFileName.Location = new System.Drawing.Point(94, 307);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 3;
+            this.txtFileName.Text = "c:\\test.avi";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBrowse.Location = new System.Drawing.Point(201, 303);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(94, 326);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoSize = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(279, 193);
+            this.panel1.Size = new System.Drawing.Size(582, 298);
             this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(589, 361);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,7 +134,129 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPause;
         private System.Windows.Forms.Panel panel1;
+
+        enum State
+        {
+            Uninit,
+            Stopped,
+            Paused,
+            Playing
+        }
+        State m_State = State.Uninit;
+        DxPlay m_play = null;
+
+        //click on start button
+        private void btnStart_Click(object sender, System.EventArgs e)
+        {
+            // If necessary, close the old file
+            if (m_State == State.Stopped)
+            {
+                // Did the filename change?
+                if (txtFileName.Text != m_play.FileName)
+                {
+                    // File name changed, close the old file
+                    m_play.Dispose();
+                    m_play = null;
+                    m_State = State.Uninit;
+                }
+            }
+
+            // If we have no file open
+            if (m_play == null)
+            {
+                try
+                {
+                    // Open the file, provide a handle to play it in
+                    m_play = new DxPlay(panel1, txtFileName.Text);
+
+                    // Let us know when the file is finished playing
+                    m_play.StopPlay += new DxPlay.DxPlayEvent(m_play_StopPlay);
+                    m_State = State.Stopped;
+                }
+                catch (COMException ce)
+                {
+                    MessageBox.Show("Failed to open file: " + ce.Message, "Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            // If we were stopped, start
+            if (m_State == State.Stopped)
+            {
+                btnStart.Text = "Stop";
+                m_play.Start();
+                btnPause.Enabled = true;
+                txtFileName.Enabled = false;
+                btnBrowse.Enabled = false;
+                m_State = State.Playing;
+            }
+
+            // If we are playing or paused, stop
+            else if (m_State == State.Playing || m_State == State.Paused)
+            {
+                m_play.Stop();
+                btnPause.Enabled = false;
+                txtFileName.Enabled = true;
+                btnBrowse.Enabled = true;
+                btnStart.Text = "Start";
+                btnPause.Text = "Pause";
+                m_State = State.Stopped;
+            }
+        }
+
+        //click on pause button
+        private void btnPause_Click(object sender, System.EventArgs e)
+        {
+            // If we are playing, pause
+            if (m_State == State.Playing)
+            {
+                m_play.Pause();
+                btnPause.Text = "Resume";
+                m_State = State.Paused;
+            }
+            // If we are paused, start
+            else
+            {
+                m_play.Start();
+                btnPause.Text = "Pause";
+                m_State = State.Playing;
+            }
+        }
+
+        //click on browse button to show a file browser
+        private void btnBrowse_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = ofd.FileName;
+            }
+        }
+
+        // Called when the video is finished playing
+        private void m_play_StopPlay(Object sender)
+        {
+            // This isn't the right way to do this, but heck, it's only a sample
+            CheckForIllegalCrossThreadCalls = false;
+
+            btnPause.Enabled = false;
+            txtFileName.Enabled = true;
+            btnBrowse.Enabled = true;
+            btnStart.Text = "Start";
+            btnPause.Text = "Pause";
+
+            CheckForIllegalCrossThreadCalls = true;
+
+            m_State = State.Stopped;
+
+            // Rewind clip to beginning to allow DxPlay.Start to work again.
+            m_play.Rewind();
+        }
     }
 }
 
