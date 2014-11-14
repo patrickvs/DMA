059ccb3442404343dce791237c08e55a53b99cd5
nraverst <nickR.Verstraete@UGent.be>
2014-11-14 14:13:16 +0100

    exercise 3, nog zonder GUI en testen

--- Solution/shot_detection_src_30/shot_detection_src_30/ShotDetection.cs
+++ Solution/shot_detection_src_30/shot_detection_src_30/ShotDetection.cs
@@ -48,5 +48,57 @@ namespace shot_detection_src_30
             shotList.Add(detectedShots[detectedShots.Count()-1] + "-" + (frameList.Count()-1));
             return shotList;
         }
+
+        public List<String> GlobalHistogramSD(double threshold, int bins)
+        {
+            List<int> detectedShots = new List<int>();
+            //The first frame is always the start of a new shot
+            detectedShots.Add(0);
+            int[] prevhistoR = getHistogram(frameList[0], bins, 0);
+            int[] prevhistoG = getHistogram(frameList[0], bins, 1);
+            int[] prevhistoB = getHistogram(frameList[0], bins, 2);
+            //loop over all frames
+            for (int i = 1; i < frameList.Count(); i++)
+            {
+                int[] curhistoR = getHistogram(frameList[i], bins, 0);
+                int[] curhistoG = getHistogram(frameList[i], bins, 1);
+                int[] curhistoB = getHistogram(frameList[i], bins, 2); 
+                //calculate diff between prev and cur
+                int diff = 0;
+                for (int j = 0; j < bins; j++)
+                { 
+                    diff += Math.Abs(prevhistoR[j] - curhistoR[j]);
+                    diff += Math.Abs(prevhistoG[j] - curhistoG[j]);
+                    diff += Math.Abs(prevhistoB[j] - curhistoB[j]);
+                }
+                //compare diff to treshold
+                if (diff > threshold)
+                    //add to list of detected shots
+                    detectedShots.Add(i);
+                // put cur in prev
+                prevhistoB = curhistoB;
+                prevhistoG = curhistoG;
+                prevhistoR = curhistoR;
+            }
+            List<String> shotList = new List<String>();
+            for (int i = 0; i < detectedShots.Count() - 1; i++)
+            {
+                shotList.Add(detectedShots[i] + "-" + (detectedShots[i + 1] - 1));
+            }
+            shotList.Add(detectedShots[detectedShots.Count() - 1] + "-" + (frameList.Count() - 1));
+            return shotList;
+        }
+
+        public int[] getHistogram(Byte[] frame, int bins, int c) {
+            int[] histogram = new int[bins];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    histogram[(frame[(y * width + x) * 3 + c]) / (int)Math.Ceiling(256.0 / bins)]++;
+                }
+            }
+            return histogram;
+        }
     }
 }
